f00c54f3763dd365278f36e3467858374a9c9082
Isaac Leonard <ifleonar@us.ibm.com>
2018-04-04 20:26:47 -0700

    back prop still not working

--- src/bin/macro_dense.rs
+++ src/bin/macro_dense.rs
@@ -1,98 +1,70 @@
 #[macro_use]
 extern crate bitnn;
 extern crate rand;
-extern crate time;
-
-use bitnn::datasets::mnist;
-
-fn dense_goodness(
-    &params: &([[u64; 13]; 2 * 64], [[u64; 2]; 10]),
-    &(input, target): &([u64; 13], usize),
-) -> i64 {
-    let mut hidden = [0u64; 2];
-    dense_bits2bits!(13, 2)(&mut hidden, &params.0, &input);
-    let mut output = [0u32; 10];
-    dense_bits2ints!(2, 10)(&mut output, &params.1, &hidden);
-    output[target] as i64
-}
-
-fn dense_acc(
-    &params: &([[u64; 13]; 2 * 64], [[u64; 2]; 10]),
-    &(input, target): &([u64; 13], usize),
-) -> i64 {
-    let mut hidden = [0u64; 2];
-    dense_bits2bits!(13, 2)(&mut hidden, &params.0, &input);
-    let mut output = [0u32; 10];
-    dense_bits2ints!(2, 10)(&mut output, &params.1, &hidden);
-    correct!(output, target) as i64
-}
-
-const TRAINING_SIZE: usize = 60000;
 
 fn main() {
-    let training_size = TRAINING_SIZE;
-    let test_size = 10000;
-    let goodness_size = 1000;
-
-    println!("loading data");
-    let images = mnist::load_images_bitpacked(
-        &String::from("mnist/train-images-idx3-ubyte"),
-        training_size,
-    );
-    let labels = mnist::load_labels(
-        &String::from("mnist/train-labels-idx1-ubyte"),
-        training_size,
-    );
-    //let test_images =
-    //    mnist::load_images_bitpacked(&String::from("mnist/train-images-idx3-ubyte"), test_size);
-    //let test_labels = mnist::load_labels(&String::from("mnist/train-labels-idx1-ubyte"), test_size);
-    //let test_examples: Vec<([u64; 13], usize)> = test_images
-    //    .iter()
-    //    .zip(test_labels.iter())
-    //    .map(|x| (*x.0, *x.1))
-    //    .collect();
-    //let mut test_set = test_examples.iter().cycle();
-
-    //let mut examples: Vec<([u64; 13], usize)> = images
-    //    .iter()
-    //    .zip(labels.iter())
-    //    .map(|x| (*x.0, *x.1))
-    //    .collect();
-    //let mut rng = rand::thread_rng();
-    //rng.shuffle(&mut examples);
-    //let mut training_set = examples.iter().cycle();
-
-    println!("initializing params"); //goodness!(outputs, target)
-
-    let mut params = ([[0u64; 13]; 2 * 64], [[0u64; 2]; 10]);
+    let mut params = [[0u64; 3]; 2 * 64];
     for o in 0..2 * 64 {
-        for i in 0..13 {
-            params.0[o][i] = rand::random::<u64>()
+        for i in 0..3 {
+            params[o][i] = rand::random::<u64>()
         }
     }
-    for o in 0..10 {
-        for i in 0..2 {
-            params.1[o][i] = rand::random::<u64>()
-        }
+    let layer1 = dense_bits2bits!(3, 2);
+    let layer1_grad = dense_bits2bits_grad!(3, 2);
+    let empirical_grad = dense_empirical_grad!(3, 2);
+    let inputs = [rand::random::<u64>(), rand::random::<u64>(), rand::random::<u64>()];
+
+    let target_grads = ([rand::random::<u64>(), rand::random::<u64>()], [!0b0u64, !0b0u64]);
+    println!("target: {:064b}", target_grads.0[0]);
+    println!("input: {:064b}", inputs[0]);
+    let mut actuals = [0u64; 2];
+    layer1(&mut actuals, &params, &inputs);
+    let grads = layer1_grad(&target_grads, &params, &inputs);
+
+    let layer_fn = |input: &[u64; 3]| -> [u64; 2] {
+        let mut outputs = [0u64; 2];
+        layer1(&mut outputs, &params, &input);
+        outputs
+    };
+    let eg = empirical_grad(&inputs, &target_grads, &layer_fn);
+    println!("{:?}", eg);
+
+    println!("grads {:064b}", grads.0[0]);
+    println!("grads {:?}", grads.0);
+    println!("mask {:064b}", grads.1[0]);
+    print!("targets:");
+    for target in target_grads.0.iter() {
+        print!(" {:064b}", target);
     }
-    let layer_1 = dense_bits2bits!(13, 2);
-    let layer1_grad = dense_bits2bits_grad!(13, 2);
-    //let state_1 = [u64; 2];
-    let layer_2 = dense_bits2ints!(2, 10);
-    let layer2_grad = dense_bits2ints_grad!(2, 10);
-    //let state_2 = [u32; 10];
-    //println!("images.len(): {:?}", images.len());
-    println!("allocing");
-    let mut outputs_weights = [0i32; 13 * 64];
-    //for e in 0..TRAINING_SIZE {
-    let e = 0;
-    let mut grads = [0i8; 10];
-    grads[labels[e]] = 1;
-    let mut state1 = [0u64; 2];
-    layer_1(&mut state1, &params.0, &images[e]);
-    let grads2 = layer2_grad(&grads, &params.1, &state1);
-    let grads1 = layer1_grad(&grads2, &params.0, &images[e]);
-    println!("grads1: {:?}", grads1[0]);
-    println!("grads2: {:?}", grads2[0]);
-    //}
+    print!("\n",);
+    print!("actuals:");
+    for target in actuals.iter() {
+        print!(" {:064b}", target);
+    }
+    print!("\n",);
+    print!("inputs:  ");
+    for input in inputs.iter() {
+        print!(" {:064b}", input);
+    }
+    print!("\n",);
+    print!("eg       ");
+    for g in eg.0.iter() {
+        print!(" {:064b}", g);
+    }
+    print!("\n",);
+    print!("eg masks:");
+    for m in eg.1.iter() {
+        print!(" {:064b}", m);
+    }
+    print!("\n",);
+    print!("cg       ");
+    for g in grads.0.iter() {
+        print!(" {:064b}", g);
+    }
+    print!("\n",);
+    print!("cg masks:");
+    for m in grads.1.iter() {
+        print!(" {:064b}", m);
+    }
+    print!("\n",);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ pub mod optimize {
     /// $goodness should be a function which depends on $word and returns a f64.
     #[macro_export]
     macro_rules! optimize_word_bits {
-        ($word:expr, $avg_goodness:expr, $goodness:expr) => (
+        ($word:expr, $avg_goodness:expr, $goodness:expr) => {
             for p in 0..64 {
                 // for each bit of the word,
                 let perturbation = 1 << p;
                 $word = $word ^ perturbation;
-                let new_goodness =  $goodness;
+                let new_goodness = $goodness;
                 if new_goodness > $avg_goodness {
                     //println!("keeping");
                     $avg_goodness = new_goodness;
@@ -20,31 +20,25 @@ pub mod optimize {
                     $word = $word ^ perturbation; // revert
                 }
             }
-        )
+        };
     }
 
     /// Expands a function which takes a single example and
     /// returns one value into a code which returns the average of n calls.
     #[macro_export]
     macro_rules! average {
-        ($training_set:expr, $sample:expr, $n:expr) => (
-            $training_set
-                .by_ref()
-                .take($n)
-                .fold(0i64, |sum, x| sum + $sample(x)) as f64
-                / $n as f64
-        )
+        ($training_set:expr, $sample:expr, $n:expr) => {
+            $training_set.by_ref().take($n).fold(0i64, |sum, x| sum + $sample(x)) as f64 / $n as f64
+        };
     }
 
     /// Converts a function which takes params and an example and
     /// returns a goodness into a function which just takes the example.
     #[macro_export]
     macro_rules! wrap_params {
-        ($params:expr, $value_func:expr, $data_type:ty) => (
-            |example: $data_type| -> i64 {
-                $value_func($params, example)
-            }
-        )
+        ($params:expr, $value_func:expr, $data_type:ty) => {
+            |example: $data_type| -> i64 { $value_func($params, example) }
+        };
     }
 }
 
@@ -81,13 +75,11 @@ pub mod datasets {
             // rev() the slice before use if you want them in the correct order.
             let mut images: Vec<[u64; 13]> = Vec::new();
             for _ in 0..size {
-                file.read_exact(&mut images_bytes)
-                    .expect("can't read images");
+                file.read_exact(&mut images_bytes).expect("can't read images");
                 let mut image_words: [u64; 13] = [0; 13];
                 for p in 0..784 {
                     let word_index = p / 64;
-                    image_words[word_index] =
-                        image_words[word_index] | (((images_bytes[p] > 128) as u64) << p % 64);
+                    image_words[word_index] = image_words[word_index] | (((images_bytes[p] > 128) as u64) << p % 64);
                 }
                 images.push(image_words);
             }
@@ -108,8 +100,7 @@ pub mod datasets {
             // rev() the slice before use if you want them in the correct order.
             let mut images: Vec<[[u64; 28]; 28]> = Vec::new();
             for _ in 0..size {
-                file.read_exact(&mut images_bytes)
-                    .expect("can't read images");
+                file.read_exact(&mut images_bytes).expect("can't read images");
                 let mut image = [[0u64; 28]; 28];
                 for p in 0..784 {
                     let mut ones = 0u64;
@@ -126,34 +117,27 @@ pub mod datasets {
     pub mod layers {
         #[macro_export]
         macro_rules! conv2d {
-            ($x_size:expr, $y_size:expr, $strides:expr) => (
-                |input: &[[u64; $x_size]; $y_size],
-                weights: &[[u64; 9]; 64]| ->
-                [[u64; ($x_size - 2) / $strides.0]; ($y_size - 2) / $strides.1] {
-                let mut output = [[0u64; ($x_size - 2) / $strides.0]; ($y_size - 2) / $strides.1];
+            ($x_size:expr, $y_size:expr, $strides:expr) => {
+                |input: &[[u64; $x_size]; $y_size], weights: &[[u64; 9]; 64]| -> [[u64; ($x_size - 2) / $strides.0]; ($y_size - 2) / $strides.1] {
+                    let mut output = [[0u64; ($x_size - 2) / $strides.0]; ($y_size - 2) / $strides.1];
                     for x in 0..($x_size - 2) / $strides.0 {
                         for y in 0..($y_size - 2) / $strides.1 {
                             for chan in 0..64 {
-        let sum = (weights[chan][0] ^ input[x * $strides.0 + 0][y * $strides.1 + 0]).count_ones()
-                + (weights[chan][1] ^ input[x * $strides.0 + 1][y * $strides.1 + 0]).count_ones()
-                + (weights[chan][2] ^ input[x * $strides.0 + 2][y * $strides.1 + 0]).count_ones()
-                + (weights[chan][3] ^ input[x * $strides.0 + 0][y * $strides.1 + 1]).count_ones()
-                + (weights[chan][4] ^ input[x * $strides.0 + 1][y * $strides.1 + 1]).count_ones()
-                + (weights[chan][5] ^ input[x * $strides.0 + 2][y * $strides.1 + 1]).count_ones()
-                + (weights[chan][6] ^ input[x * $strides.0 + 0][y * $strides.1 + 2]).count_ones()
-                + (weights[chan][7] ^ input[x * $strides.0 + 1][y * $strides.1 + 2]).count_ones()
-                + (weights[chan][8] ^ input[x * $strides.0 + 2][y * $strides.1 + 2]).count_ones();
+                                let sum = (weights[chan][0] ^ input[x * $strides.0 + 0][y * $strides.1 + 0]).count_ones() + (weights[chan][1] ^ input[x * $strides.0 + 1][y * $strides.1 + 0]).count_ones() + (weights[chan][2] ^ input[x * $strides.0 + 2][y * $strides.1 + 0]).count_ones()
+                                    + (weights[chan][3] ^ input[x * $strides.0 + 0][y * $strides.1 + 1]).count_ones() + (weights[chan][4] ^ input[x * $strides.0 + 1][y * $strides.1 + 1]).count_ones()
+                                    + (weights[chan][5] ^ input[x * $strides.0 + 2][y * $strides.1 + 1]).count_ones() + (weights[chan][6] ^ input[x * $strides.0 + 0][y * $strides.1 + 2]).count_ones()
+                                    + (weights[chan][7] ^ input[x * $strides.0 + 1][y * $strides.1 + 2]).count_ones() + (weights[chan][8] ^ input[x * $strides.0 + 2][y * $strides.1 + 2]).count_ones();
                                 output[x][y] = output[x][y] | (((sum > 288) as u64) << chan);
                             }
                         }
                     }
                     return output;
                 }
-            )
+            };
         }
         #[macro_export]
         macro_rules! dense_bits2bits {
-            ($input_size:expr, $output_size:expr) => (
+            ($input_size:expr, $output_size:expr) => {
                 |output: &mut [u64; $output_size], weights: &[[u64; $input_size]; $output_size * 64], input: &[u64; $input_size]| {
                     for o in 0..$output_size * 64 {
                         let mut sum = 0;
@@ -161,76 +145,147 @@ pub mod datasets {
                             sum += (weights[o][i] ^ input[i]).count_ones();
                         }
                         let word_index = o / 64;
-                        output[word_index] = output[word_index] |
-                        (((sum > $input_size * 64 / 2) as u64) << o % 64);
+                        output[word_index] = output[word_index] | (((sum > $input_size * 64 / 2) as u64) << o % 64);
                     }
                 }
-            )
+            };
         }
+        /// create a function to compute the passthrough grad.
         #[macro_export]
         macro_rules! dense_bits2bits_grad {
-            ($input_size:expr, $output_size:expr) => (
-                |target_grad: &[i8; $output_size * 64], weights: &[[u64; $input_size]; $output_size * 64], input: &[u64; $input_size]| -> [i8; $input_size * 64] {
+            ($input_size:expr, $output_size:expr) => {
+                |&(target_grad, target_mask): &([u64; $output_size], [u64; $output_size]), params: &[[u64; $input_size]; $output_size * 64], input: &[u64; $input_size]| -> ([u64; $input_size], [u64; $input_size]) {
                     let threshold = ($input_size * 64) / 2;
-                    let mut input_sums = [0i32; $input_size * 64];
+                    //let mut input_sums = [0i32; $input_size * 64];
+                    let mut internal_grads = [[0u64; $input_size]; $output_size * 64];
+                    let mut internal_masks = [[0u64; $input_size]; $output_size * 64];
+                    let mut sums = [0i32; $input_size * 64];
                     for output_word in 0..$output_size {
-                        for output_word_bit in 0..64 {
-                            let mut xored_bits = [0u64; $input_size];
-                            let mut sum = 0;
-                            for i in 0..$input_size {
-                                xored_bits[i] = input[i] ^ weights[output_word * output_word_bit][i];
-                                sum += xored_bits[i].count_ones();
-                            }
-                            if target_grad[output_word * output_word_bit] > 0 {
-                                if sum == threshold {
-                                    for input_word in 0..$input_size {
-                                        for input_word_bit in 0..64 {
-                                            if ((xored_bits[input_word] >> input_word_bit) & 0b1) == 0b1 {
-                                                input_sums[input_word * input_word_bit] += 1;
+                        // if the mask is all 0, we can skip the whole word.
+                        if target_mask[output_word] != 0b0u64 {
+                            // now for each bit of that word of the target.
+                            for output_word_bit in 0..64 {
+                                let target_mask_bit = ((target_mask[output_word] >> output_word_bit) & 0b1) == 0b1;
+                                // if the mask is 0, we can skip the rest, no one cares what the answer is.
+                                if target_mask_bit {
+                                    let target_grad_bit = ((target_grad[output_word] >> output_word_bit) & 0b1) == 0b1;
+                                    let o = output_word * output_word_bit;
+                                    let mut xored_bits = [0u64; $input_size]; // for each output, the input xored with the corresponding params.
+                                    let mut sum = 0;
+                                    for i in 0..$input_size {
+                                        xored_bits[i] = input[i] ^ params[o][i];
+                                        sum += xored_bits[i].count_ones();
+                                    }
+                                    if target_grad_bit & (sum == threshold) {
+                                        for input_word in 0..$input_size {
+                                            for iwb in 0..64 {
+                                                if ((xored_bits[input_word] >> iwb) & 0b1) == 0b1 {
+                                                    sums[input_word * iwb] += 1;
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            }
-                            if sum == (threshold + 1) {
-                                for input_word in 0..$input_size {
-                                    for input_word_bit in 0..64 {
-                                        if ((xored_bits[input_word] >> input_word_bit) & 0b1) == 0b1 {
-                                            input_sums[input_word * input_word_bit] += -1;
+                                    // if the target grad is negative, and the sum is one greater then the threshold, we want one more bit to be unset.
+                                    if !target_grad_bit & (sum == (threshold + 1)) {
+                                        //println!("{:?} {:?}, {:?}", target_grad_bit, sum, threshold);
+                                        for input_word in 0..$input_size {
+                                            for iwb in 0..64 {
+                                                if ((xored_bits[input_word] >> iwb) & 0b1) == 0b0 {
+                                                    sums[input_word * iwb] += -1;
+                                                }
+                                            }
                                         }
+
                                     }
                                 }
                             }
                         }
                     }
-                    let mut grads = [0i8; $input_size * 64];
-                    for (index, &sum) in input_sums.iter().enumerate() {
-                        if sum > 0 {
-                            grads[index] = 1;
-                        }
-                        if sum < 0 {
-                            grads[index] = -1;
+                    for sum in sums.iter() {
+                        print!("{:?} ", sum);
+                    }
+                    println!("\n");
+                    // (grads, masks)
+                    let mut grads = ([0u64; $input_size], [0u64; $input_size]);
+                    for input_word in 0..$input_size {
+                        // for each word of the input,
+                        for input_word_bit in 0..64 {
+                            //println!("sum {:?}", sums[input_word * input_word_bit]);
+                            if sums[input_word * input_word_bit] > 0 {
+                                //println!("setting mask to {:?}", 0b1);
+                                grads.0[input_word] = grads.0[input_word] | 0b1 << input_word_bit;
+                                grads.1[input_word] = grads.1[input_word] | 0b1 << input_word_bit;
+                            }
+                            if sums[input_word * input_word_bit] < 0 {
+                                //println!("{:?}", grads.1[input_word]);
+                                //grads.0[input_word] = grads.0[input_word] | 0b0 << input_word_bit;
+                                grads.1[input_word] = grads.1[input_word] | 0b1 << input_word_bit;
+                            }
                         }
                     }
                     grads
                 }
-            )
+            };
+        }
+        // empirically observe the passthrough grad. (expensive)
+        #[macro_export]
+        macro_rules! dense_empirical_grad {
+            ($input_size:expr, $output_size:expr) => {
+                |&input: &[u64; $input_size], &targets: &([u64; $output_size], [u64; $output_size]), layer: &Fn(&[u64; $input_size]) -> [u64; $output_size]| -> ([u64; $input_size], [u64; $input_size]) {
+                    let nil_loss = {
+                        let outputs = layer(&input);
+                        println!("layer outputs: {:064b} {:064b}", outputs[0], outputs[1]);
+                        let mut loss = 0;
+                        for o in 0..$output_size {
+                            loss += (outputs[o] ^ targets.0[o]).count_ones();
+                        }
+                        loss
+                    };
+                    println!("nil_loss: {:?}", nil_loss);
+                    let mut scratch_input = input;
+                    let mut input_deltas = [0u64; $input_size];
+                    let mut grads = [0u64; $input_size];
+                    let mut masks = [0u64; $input_size];
+                    for input_word in 0..$input_size {
+                        for input_word_bit in 0..64 {
+                            let bitmask = 0b1 << input_word_bit;
+                            // flip one bit
+                            scratch_input[input_word] = scratch_input[input_word] ^ bitmask;
+                            let outputs = layer(&scratch_input);
+                            scratch_input[input_word] = input[input_word]; // reset it
+                            let mut loss = 0u32;
+                            for o in 0..$output_size {
+                                loss += (outputs[o] ^ targets.0[o]).count_ones();
+                            }
+                            let delta = nil_loss as i32 - loss as i32;
+                            //println!("delta: {:?} {:?} {:?}", delta, nil_loss, loss);
+                            if delta > 0 {
+                                grads[input_word] = grads[input_word] | bitmask;
+                            }
+                            if delta != 0 {
+                                masks[input_word] = masks[input_word] | bitmask;
+                            }
+                        }
+                    }
+                    println!("grads {:064b}", grads[0]);
+                    println!("masks {:064b}", masks[0]);
+                    (grads, masks)
+                }
+            };
         }
         #[macro_export]
         macro_rules! dense_bits2ints {
-            ($input_size:expr, $output_size:expr) => (
+            ($input_size:expr, $output_size:expr) => {
                 |output: &mut [u32; $output_size], params: &[[u64; $input_size]; $output_size], input: &[u64; $input_size]| {
                     for o in 0..$output_size {
-                        output[o] = input.iter().
-                        zip(params[o].iter()).
-                        fold(0, |acc, x| acc + (x.0 ^ x.1).count_ones());
+                        output[o] = input.iter().zip(params[o].iter()).fold(0, |acc, x| acc + (x.0 ^ x.1).count_ones());
                     }
                 }
-            )
+            };
         }
         #[macro_export]
         macro_rules! dense_bits2ints_grad {
-            ($input_size:expr, $output_size:expr) => (
+            ($input_size:expr, $output_size:expr) => {
                 |target_grad: &[i8; $output_size], weights: &[[u64; $input_size]; $output_size], input: &[u64; $input_size]| -> [i8; $input_size * 64] {
                     let threshold = ($input_size * 64) / 2;
                     let mut input_sums = [0i32; $input_size * 64];
@@ -271,21 +326,19 @@ pub mod datasets {
                     }
                     grads
                 }
-            )
+            };
         }
         #[macro_export]
         macro_rules! correct {
-            ($input:expr, $target:expr) => (
-                ($input.iter().enumerate().
-                max_by_key(|&(_, item)| item).unwrap().0 == $target)
-            )
+            ($input:expr, $target:expr) => {
+                ($input.iter().enumerate().max_by_key(|&(_, item)| item).unwrap().0 == $target)
+            };
         }
         #[macro_export]
         macro_rules! goodness {
-            ($input:expr, $target:expr) => (
-                $input.iter().enumerate().
-                fold(0, |acc, (index, value)| if index == $target {acc + value} else {acc})
-            )
+            ($input:expr, $target:expr) => {
+                $input.iter().enumerate().fold(0, |acc, (index, value)| if index == $target { acc + value } else { acc })
+            };
         }
     }
 }
