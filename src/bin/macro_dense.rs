27b8c56f810703f412787aaa069acfc6193ebfd4
Isaac Leonard <ifleonar@us.ibm.com>
2018-04-06 09:43:19 -0700

    Still broken

--- src/bin/macro_dense.rs
+++ src/bin/macro_dense.rs
@@ -2,69 +2,96 @@
 extern crate bitnn;
 extern crate rand;
 
+use std::time::SystemTime;
+use bitnn::datasets::mnist;
+
+//#[derive(PartialEq, Eq, Hash)]
+
+const TRAINING_SIZE: usize = 60000;
+const h1: usize = 2;
+const h2: usize = 8;
+
+
 fn main() {
-    let mut params = [[0u64; 3]; 2 * 64];
-    for o in 0..2 * 64 {
-        for i in 0..3 {
-            params[o][i] = rand::random::<u64>()
+    let test_size = 1000;
+    let onval = 128u32;
+
+    let images = mnist::load_images_bitpacked(&String::from("mnist/train-images-idx3-ubyte"), TRAINING_SIZE);
+    let labels = mnist::load_labels_onehot(&String::from("mnist/train-labels-idx1-ubyte"), TRAINING_SIZE, onval);
+    //let test_images = mnist::load_images_bitpacked(&String::from("mnist/train-images-idx3-ubyte"), test_size);
+    //let test_labels = mnist::load_labels_onehot(&String::from("mnist/train-labels-idx1-ubyte"), test_size, onval);
+
+    let mut params = ([[0u64; 13]; h1 * 64], [[0u64; h1]; h2 * 64], [[0u64; h2]; 10]);
+    for o in 0..h1 * 64 {
+        for i in 0..13 {
+            params.0[o][i] = rand::random::<u64>()
         }
     }
-    let layer1 = dense_bits2bits!(3, 2);
-    let layer1_grad = dense_bits2bits_grad!(3, 2);
-    let empirical_grad = dense_empirical_grad!(3, 2);
-    let inputs = [rand::random::<u64>(), rand::random::<u64>(), rand::random::<u64>()];
+    for o in 0..h2 * 64 {
+        for i in 0..h1 {
+            params.1[o][i] = rand::random::<u64>()
+        }
+    }
+    for o in 0..10 {
+        for i in 0..h2 {
+            params.2[o][i] = rand::random::<u64>()
+        }
+    }
+    let layer1 = dense_bits2bits!(13, h1);
+    let layer2 = dense_bits2bits!(h1, h2);
+    let layer3 = dense_bits2ints!(h2, 10);
+    let loss = int_loss!(10);
 
-    let target_grads = ([rand::random::<u64>(), rand::random::<u64>()], [!0b0u64, !0b0u64]);
-    println!("target: {:064b}", target_grads.0[0]);
-    println!("input: {:064b}", inputs[0]);
-    let mut actuals = [0u64; 2];
-    layer1(&mut actuals, &params, &inputs);
-    let grads = layer1_grad(&target_grads, &params, &inputs);
-
-    let layer_fn = |input: &[u64; 3]| -> [u64; 2] {
-        let mut outputs = [0u64; 2];
-        layer1(&mut outputs, &params, &input);
-        outputs
+    let layer1_loss = |input: &[u64; 13], targets: &[u32; 10], layer_params: &[[u64; 13]; h1 * 64], cache: &(&[u64; h1], &[u64; h2], &[u32; 10], u32)| -> u32 {
+        let mut state1 = [0u64; h1];
+        layer1(&mut state1, layer_params, &input);
+        if state1 == *cache.0 {
+            return cache.3;
+        }
+        let mut state2 = [0u64; h2];
+        layer2(&mut state2, &params.1, &state1);
+        let mut state3 = [0u32; 10];
+        layer3(&mut state3, &params.2, &state2);
+        loss(&state3, &targets)
     };
-    let eg = empirical_grad(&inputs, &target_grads, &layer_fn);
-    println!("{:?}", eg);
+    //let layer2_loss = |input: &[u64; 2], targets: &[u32; 10], layer_params: &[[u64; 2]; 10]| -> u32 {
+    //    let mut state2 = [0u32; 10];
+    //    layer2(&mut state2, &layer_params, &input);
+    //    loss(&state2, &targets)
+    //};
 
-    println!("grads {:064b}", grads.0[0]);
-    println!("grads {:?}", grads.0);
-    println!("mask {:064b}", grads.1[0]);
-    print!("targets:");
-    for target in target_grads.0.iter() {
-        print!(" {:064b}", target);
+    let mut state_cache: Vec<_> = vec![(&[0u64; h1], &[0u64; h2], &[0u32; 10], 0u32); TRAINING_SIZE];
+
+    for e in 0..TRAINING_SIZE {
+         layer1(&mut state_cache[e].0, &params.0, &images[e]);
+         layer2(&mut state_cache[e].1, &params.1, &state_cache[e].0);
+         layer3(&mut state_cache[e].2, &params.2, &state_cache[e].1);
+         state_cache[e].3 = loss(&state_cache[e].2, &labels[e]);
     }
-    print!("\n",);
-    print!("actuals:");
-    for target in actuals.iter() {
-        print!(" {:064b}", target);
+    let sum_nil_loss: u64 = images
+        .iter()
+        .zip(labels.iter()).zip(state_cache.iter())
+        .map(|((image, target), cache)| layer1_loss(image, target, &params.0, cache) as u64)
+        .sum();
+    let nil_loss = sum_nil_loss as f64 / TRAINING_SIZE as f64;
+    println!("avg nil loss: {:?}", nil_loss);
+    let mut scratch_params0 = params.0;
+    let mut losses0 = [[0f64; 13 * 64]; h1 * 64];
+    for o in 0..h1 * 64 {
+        for i in 0..13 {
+            let start = SystemTime::now();
+            for b in 0..64 {
+                scratch_params0[o][i] = scratch_params0[o][i] ^ 0b1u64 << b;
+                let sum_nil_loss: u64 = images
+                    .iter()
+                    .zip(labels.iter()).zip(state_cache.iter())
+                    .map(|((image, target), cache)| layer1_loss(image, target, &scratch_params0, cache) as u64)
+                    .sum();
+                losses0[o][i * b] = nil_loss - (sum_nil_loss as f64 / TRAINING_SIZE as f64);
+                scratch_params0[o][i] = params.0[o][i];
+                println!("{:?} {:?} {:?} delta: {:?}", o, i, b, losses0[o][i * b]);
+            }
+            println!("time for 64: {:?}", start.elapsed().unwrap());
+        }
     }
-    print!("\n",);
-    print!("inputs:  ");
-    for input in inputs.iter() {
-        print!(" {:064b}", input);
-    }
-    print!("\n",);
-    print!("eg       ");
-    for g in eg.0.iter() {
-        print!(" {:064b}", g);
-    }
-    print!("\n",);
-    print!("eg masks:");
-    for m in eg.1.iter() {
-        print!(" {:064b}", m);
-    }
-    print!("\n",);
-    print!("cg       ");
-    for g in grads.0.iter() {
-        print!(" {:064b}", g);
-    }
-    print!("\n",);
-    print!("cg masks:");
-    for m in grads.1.iter() {
-        print!(" {:064b}", m);
-    }
-    print!("\n",);
 }
