cea84695b77ad79bce16dd1fe84e7a624e885a2e
Isaac Leonard <ifleonar@us.ibm.com>
2018-08-19 19:36:13 -0700

    mnist boosted fc can now get ~53% on layer 2

--- src/bin/boosted_fc.rs
+++ src/bin/boosted_fc.rs
@@ -7,23 +7,6 @@ use bitnn::datasets::mnist;
 use rayon::prelude::*;
 use time::PreciseTime;
 
-macro_rules! bitpack_u64_2d {
-    ($name:ident, $type:ty, $a_size:expr, $b_size:expr, $thresh:expr) => {
-        fn $name(grads: &[[[$type; 64]; $b_size]; $a_size]) -> [[[u64; $c_size]; $b_size]; $a_size] {
-            let mut params = [[[0u64; $c_size]; $b_size]; $a_size];
-            for a in 0..$a_size {
-                for b in 0..$b_size {
-                    for i in 0..64 {
-                        let bit = grads[a][b][i] > $thresh;
-                        params[a][b] = params[a][b] | ((bit as u64) << i);
-                    }
-                }
-            }
-            params
-        }
-    };
-}
-
 macro_rules! fc_infer {
     ($in_size:expr, $weights:expr, $input:expr) => {{
         let mut sum = 0;
@@ -41,7 +24,7 @@ macro_rules! fc_is_correct {
             for o in 0..$n_labels {
                 if o != label {
                     let sum = fc_infer!($in_size, weights[o], input);
-                    if sum > target_sum {
+                    if sum >= target_sum {
                         //println!("{:?} {:?}", target_sum, sum);
                         return false;
                     }
@@ -131,16 +114,120 @@ macro_rules! fc_boosted_features {
     };
 }
 
+macro_rules! reflow {
+    ($name:ident, $in_size:expr, $n_labels:expr, $boostings:expr, $output_words:expr) => {
+        fn $name(input: &[[[u64; $in_size]; $n_labels]; $boostings]) -> [[[u64; $in_size]; 64]; $output_words] {
+            let mut output = [[[0u64; $in_size]; 64]; $output_words];
+            for o in 0..$output_words {
+                for b in 0..64 {
+                    let index = (o * 64) + b;
+                    output[o][b] = input[index / $n_labels][index % $n_labels];
+                }
+            }
+            output
+        }
+    };
+}
+
+macro_rules! fused_xor_popcount_threshold_and_bitpack {
+    ($name:ident, $in_size:expr, $out_size:expr) => {
+        fn $name(input: &[u64; $in_size], weights: &[[[u64; $in_size]; 64]; $out_size], thresholds: &[[u32; 64]; $out_size]) -> [u64; $out_size] {
+            let mut output = [0u64; $out_size];
+            for o in 0..$out_size {
+                for b in 0..64 {
+                    let bit = fc_infer!($in_size, weights[o][b], input) > thresholds[o][b];
+                    output[o] = output[o] | ((bit as u64) << b);
+                }
+            }
+            output
+        }
+    };
+}
+
+macro_rules! activation_medians {
+    ($name:ident, $in_size:expr, $out_size:expr) => {
+        fn $name(inputs: &Vec<[u64; $in_size]>, weights: &[[[u64; $in_size]; 64]; $out_size]) -> [[u32; 64]; $out_size] {
+            let mut output = [[0u32; 64]; $out_size];
+            for o in 0..$out_size {
+                for b in 0..64 {
+                    let mut sums: Vec<u32> = inputs.par_iter().map(|input| fc_infer!($in_size, weights[o][b], input)).collect();
+                    sums.sort();
+                    output[o][b] = sums[inputs.len() / 2];
+                }
+            }
+            output
+        }
+    };
+}
+
 const TRAIN_SIZE: usize = 60000;
 
-fc_boosted_features!(boosted_grads, 13, 10, 20);
+const L1_B: usize = 14;
+const L2_W: usize = 2;
+const L2_B: usize = 20;
+
+fc_is_correct!(l1_is_correct, 13, 10);
+fc_boosted_features!(l1_boosted_features, 13, 10, L1_B);
+reflow!(l1_reflow, 13, 10, L1_B, L2_W);
+activation_medians!(l1_medians, 13, 2);
+fused_xor_popcount_threshold_and_bitpack!(l1_fxoptbp, 13, L2_W);
+
+
+fc_is_correct!(l2_is_correct, 2, 10);
+fc_boosted_features!(l2_boosted_features, 2, 10, L2_B);
+//reflow!(l2_reflow, 2, 10, L2_B, L2_W);
+//fused_xor_popcount_and_bitpack!(l2_xopcbp, 13, L2_W);
 
 fn main() {
     let images = mnist::load_images_bitpacked(&String::from("/home/isaac/big/cache/datasets/mnist/train-images-idx3-ubyte"), TRAIN_SIZE);
     let labels = mnist::load_labels(&String::from("/home/isaac/big/cache/datasets/mnist/train-labels-idx1-ubyte"), TRAIN_SIZE);
-    let examples: Vec<(usize, [u64; 13])> = labels.iter().zip(images.iter()).map(|(&label, &image)| (label as usize, image)).collect();
-    let l1_weights = boosted_grads(&examples);
-    for weights in &l1_weights {
-        println!("{:064b}", weights[0][4]);
+    let l1_examples: Vec<(usize, [u64; 13])> = labels.iter().zip(images.iter()).map(|(&label, &image)| (label as usize, image)).collect();
+
+    let test_images = mnist::load_images_bitpacked(&String::from("/home/isaac/big/cache/datasets/mnist/t10k-images-idx3-ubyte"), 10000);
+    let test_labels = mnist::load_labels(&String::from("/home/isaac/big/cache/datasets/mnist/t10k-labels-idx1-ubyte"), 10000);
+    let test_examples: Vec<(usize, [u64; 13])> = test_labels
+        .iter()
+        .zip(test_images.iter())
+        .map(|(&label, &image)| (label as usize, image))
+        .collect();
+
+    let weights = l1_boosted_features(&l1_examples);
+    let l1_features = l1_reflow(&weights);
+    let thresholds = l1_medians(&images, &l1_features);
+    for i in 0..L1_B {
+        let total_correct: u64 = test_examples
+            .par_iter()
+            .map(|(label, image)| l1_is_correct(&image, *label, weights[i]))
+            .map(|x| x as u64)
+            .sum();
+        let avg_correct = total_correct as f32 / test_examples.len() as f32;
+        println!("{:?} acc: {:?}%", i, avg_correct * 100f32);
+    }
+    let l2_images: Vec<[u64; 2]> = images.par_iter().map(|image| l1_fxoptbp(&image, &l1_features, &thresholds)).collect();
+    let l2_examples: Vec<(usize, [u64; 2])> = labels
+        .iter()
+        .zip(l2_images.iter())
+        .map(|(&label, &image)| (label as usize, image))
+        .collect();
+
+    let l2_test_images: Vec<[u64; 2]> = test_images.par_iter().map(|image| l1_fxoptbp(&image, &l1_features, &thresholds)).collect();
+    let l2_test_examples: Vec<(usize, [u64; 2])> = test_labels
+        .iter()
+        .zip(l2_test_images.iter())
+        .map(|(&label, &image)| (label as usize, image))
+        .collect();
+
+    println!("images: {:?}", &l2_images[0..20]);
+
+    let weights = l2_boosted_features(&l2_examples);
+    //let l2_features = l1_reflow(&weights);
+    for i in 0..L2_B {
+        let total_correct: u64 = l2_test_examples
+            .par_iter()
+            .map(|(label, image)| l2_is_correct(&image, *label, weights[i]))
+            .map(|x| x as u64)
+            .sum();
+        let avg_correct = total_correct as f32 / test_examples.len() as f32;
+        println!("{:?} acc: {:?}%", i, avg_correct * 100f32);
     }
 }
