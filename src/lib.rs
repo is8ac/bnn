505c5c7cdb2de488c99be3c10a1736077d1ccbeb
Isaac Leonard <ifleonar@us.ibm.com>
2018-11-08 09:44:22 -0800

    added WeightsMatrix trait

--- src/lib.rs
+++ src/lib.rs
@@ -414,7 +414,7 @@ pub mod featuregen {
 pub mod layers {
     use rayon::prelude::*;
 
-    pub trait Patch {
+    pub trait Patch: Send + Sync {
         fn hamming_distance(&self, &Self) -> u32;
         fn bit_increment(&self, &mut [u32]);
         fn bit_len() -> usize;
@@ -569,6 +569,85 @@ pub mod layers {
     array_patch!(49);
     array_patch!(64);
 
+    pub trait WeightsMatrix<I: Patch, O: Patch> {
+        fn mul(&self, &I) -> O;
+    }
+
+    macro_rules! primitive_weights_matrix {
+        ($type:ty, $len:expr) => {
+            impl<I: Patch> WeightsMatrix<I, $type> for [(I, u32); $len] {
+                fn mul(&self, input: &I) -> $type {
+                    let mut val = 0 as $type;
+                    for i in 0..$len {
+                        val = val | (((self[i].0.hamming_distance(&input) > self[i].1) as $type) << i);
+                    }
+                    val
+                }
+            }
+        };
+    }
+
+    primitive_weights_matrix!(u8, 8);
+    primitive_weights_matrix!(u16, 16);
+    primitive_weights_matrix!(u32, 32);
+    primitive_weights_matrix!(u64, 64);
+    primitive_weights_matrix!(u128, 128);
+
+    macro_rules! primitive_weights_matrix_unary {
+        ($type:ty, $len:expr, $unary_bits:expr) => {
+            impl<I: Patch> WeightsMatrix<I, [$type; $unary_bits]> for [(I, [u32; $unary_bits]); $len] {
+                fn mul(&self, input: &I) -> [$type; $unary_bits] {
+                    let mut val = [0 as $type; $unary_bits];
+                    for i in 0..$len {
+                        let dist = self[i].0.hamming_distance(&input);
+                        for b in 0..$unary_bits {
+                            val[b] = val[b] | (((dist > self[i].1[b]) as $type) << i);
+                        }
+                    }
+                    val
+                }
+            }
+        };
+    }
+
+    macro_rules! primitive_weights_matrix_n_bit_types {
+        ($unary_bits:expr) => {
+            primitive_weights_matrix_unary!(u8, 8, $unary_bits);
+            primitive_weights_matrix_unary!(u16, 16, $unary_bits);
+            primitive_weights_matrix_unary!(u32, 32, $unary_bits);
+            primitive_weights_matrix_unary!(u64, 64, $unary_bits);
+            primitive_weights_matrix_unary!(u128, 128, $unary_bits);
+        };
+    }
+
+    primitive_weights_matrix_n_bit_types!(2);
+    primitive_weights_matrix_n_bit_types!(3);
+    primitive_weights_matrix_n_bit_types!(4);
+    primitive_weights_matrix_n_bit_types!(5);
+    primitive_weights_matrix_n_bit_types!(6);
+    primitive_weights_matrix_n_bit_types!(7);
+    primitive_weights_matrix_n_bit_types!(8);
+
+    macro_rules! array_weights_matrix {
+        ($len:expr) => {
+            impl<I: Patch, T: Copy + Default + Patch, O: WeightsMatrix<I, T>> WeightsMatrix<I, [T; $len]> for [O; $len] {
+                fn mul(&self, input: &I) -> [T; $len] {
+                    let mut val = [T::default(); $len];
+                    for i in 0..$len {
+                        val[i] = self[i].mul(&input);
+                    }
+                    val
+                }
+            }
+        };
+    }
+
+    array_weights_matrix!(2);
+    array_weights_matrix!(3);
+    array_weights_matrix!(4);
+
+
+
     pub mod bitvecmul {
         macro_rules! primitive_bit_vecmul {
             ($name:ident, $type:ty, $len:expr) => {
@@ -591,10 +670,10 @@ pub mod layers {
         primitive_bit_vecmul!(bvm_u64, u64, 64);
         primitive_bit_vecmul!(bvm_u128, u128, 128);
 
-         // Vec Masked Bit Vector Multiply
-         pub fn vbvm<T: super::Patch>(weights: &Vec<T>, input: &T) -> Vec<u32> {
-             weights.iter().map(|signs| input.hamming_distance(&signs)).collect()
- }
+        // Vec Masked Bit Vector Multiply
+        pub fn vbvm<T: super::Patch>(weights: &Vec<T>, input: &T) -> Vec<u32> {
+            weights.iter().map(|signs| input.hamming_distance(&signs)).collect()
+        }
 
     }
 
@@ -762,6 +841,7 @@ pub mod layers {
 
     pub mod pixelmap {
         use super::Patch;
+        use featuregen;
         macro_rules! pixel_map_2d {
             ($name:ident, $x_size:expr, $y_size:expr) => {
                 pub fn $name<I: Copy, O: Copy + Default>(input: &[[I; $y_size]; $x_size], map_fn: &Fn(I) -> O) -> [[O; $y_size]; $x_size] {
@@ -1348,6 +1428,130 @@ pub mod layers {
             }
             array
         }};
+    }
+    pub mod readout {
+        use super::Patch;
+        use featuregen;
+        use rayon::prelude::*;
+
+        pub struct ReadoutHead10<T: Patch> {
+            weights: [T; 10],
+            biases: [i32; 10],
+        }
+
+        impl<T: Patch + Default + Copy + Sync + Send> ReadoutHead10<T> {
+            pub fn acc(&self, examples: &Vec<(usize, T)>) -> f64 {
+                let test_correct: u64 = examples
+                    .par_iter()
+                    .map(|input| {
+                        (input.0 == self
+                            .weights
+                            .iter()
+                            .zip(self.biases.iter())
+                            .map(|(base_point, bias)| base_point.hamming_distance(&input.1) as i32 - bias)
+                            .enumerate()
+                            .max_by_key(|(_, dist)| *dist)
+                            .unwrap()
+                            .0) as u64
+                    }).sum();
+                test_correct as f64 / examples.len() as f64
+            }
+            pub fn new_from_split(examples: &Vec<(usize, T)>) -> Self {
+                let by_class = featuregen::split_by_label(&examples, 10);
+                let mut readout = ReadoutHead10 {
+                    weights: [T::default(); 10],
+                    biases: [0i32; 10],
+                };
+                for class in 0..10 {
+                    let grads = featuregen::grads_one_shard(&by_class, class);
+                    let sign_bits: Vec<bool> = grads.iter().map(|x| *x > 0f64).collect();
+                    readout.weights[class] = T::bitpack(&sign_bits);
+                    let sum_activation: u64 = examples.iter().map(|(_, input)| readout.weights[class].hamming_distance(&input) as u64).sum();
+                    readout.biases[class] = (sum_activation as f64 / examples.len() as f64) as i32;
+                }
+                readout
+            }
+            pub fn bitwise_ascend_acc(&mut self, examples: &Vec<(usize, T)>) {
+                for mut_class in 0..10 {
+                    let mut activation_diffs: Vec<(T, i32, bool)> = examples
+                        .iter()
+                        .map(|(targ_class, input)| {
+                            let mut activations: Vec<i32> = self
+                                .weights
+                                .iter()
+                                .zip(self.biases.iter())
+                                .map(|(base_point, bias)| base_point.hamming_distance(&input) as i32 - bias)
+                                .collect();
+
+                            let targ_act = activations[*targ_class]; // the activation for target class of this example.
+                            let mut_act = activations[mut_class]; // the activation which we are mutating.
+                            activations[*targ_class] = -10000;
+                            activations[mut_class] = -10000;
+                            let max_other_activations = activations.iter().max().unwrap(); // the max activation of all the classes not in the target class or mut class.
+                            let diff = {
+                                if *targ_class == mut_class {
+                                    mut_act - max_other_activations
+                                } else {
+                                    mut_act - targ_act
+                                }
+                            };
+                            (input, diff, *targ_class == mut_class, (targ_act > *max_other_activations) | (*targ_class == mut_class)) // diff betwene the activation of the
+                        }).filter(|(_, _, _, keep)| *keep)
+                        .map(|(input, diff, sign, _)| (*input, diff, sign))
+                        .collect();
+
+                    // note that this sum correct is not the true acc, it is working on the subset that can be made correct or incorrect by this activation.
+                    let mut sum_correct: i64 = activation_diffs
+                        .par_iter()
+                        .map(|(_, diff, sign)| {
+                            if *sign {
+                                // if we want the mut_act to be bigger,
+                                *diff > 0 // count those which are bigger,
+                            } else {
+                                // otherwise,
+                                *diff < 0 // count those which are smaller.
+                            }
+                        } as i64).sum();
+                    for b in 0..T::bit_len() {
+                        // the new weights bit
+                        let new_weights_bit = !self.weights[mut_class].get_bit(b);
+                        // if we were to flip the bit of the weights,
+                        let new_sum_correct: i64 = activation_diffs
+                            .par_iter()
+                            .map(|(input, diff, sign)| {
+                                // new diff is the diff after flipping the weights bit
+                                let new_diff = {
+                                    if input.get_bit(b) ^ new_weights_bit {
+                                        // flipping the bit would make mut_act larger
+                                        diff + 2
+                                    } else {
+                                        diff - 2
+                                    }
+                                };
+                                // do we want mut_act to be smaller or larger?
+                                // same as this statement:
+                                //(if *sign { new_diff > 0 } else { new_diff < 0 }) as i64
+                                ((*sign ^ (new_diff < 0)) & (new_diff != 0)) as i64
+                            }).sum();
+                        if new_sum_correct > sum_correct {
+                            sum_correct = new_sum_correct;
+                            // actually flip the bit
+                            self.weights[mut_class].flip_bit(b);
+                            // now update each
+                            activation_diffs
+                                .par_iter_mut()
+                                .map(|i| {
+                                    if i.0.get_bit(b) ^ new_weights_bit {
+                                        i.1 += 2;
+                                    } else {
+                                        i.1 -= 2;
+                                    }
+                                }).collect::<Vec<_>>();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
