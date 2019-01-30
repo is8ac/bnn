579373fe91f0f1b187037be48a61a0b8efe8e4e5
Isaac Leonard <ifleonar@us.ibm.com>
2019-01-30 08:05:03 -0800

    DenseNet-B test

--- src/bin/cifar_batch_test.rs
+++ src/bin/cifar_batch_test.rs
@@ -4,9 +4,11 @@ extern crate rayon;
 extern crate time;
 use bitnn::datasets::cifar;
 use bitnn::layers::unary;
+use bitnn::layers::vec_concat_examples;
 use bitnn::layers::{
-    Conv2x2Stride2, Conv3x3, ExtractPixels, Image2D, MakePixelHead, NewFromSplit, Objective,
-    OptimizeHead, OptimizeLayer, Patch3x3NotchedConv, PixelHead10, PixelMap, SaveLoad, VecApply,
+    Conv1x1, Conv2x2Stride2, Conv3x3, ExtractPixels, Image2D, MakePixelHead, NewFromSplit,
+    Objective, OptimizeHead, OptimizeLayer, Patch3x3NotchedConv, PixelHead10, PixelMap, SaveLoad,
+    VecApply,
 };
 use bitnn::Patch;
 use std::fs::OpenOptions;
@@ -34,13 +36,7 @@ fn load_data() -> Vec<(usize, [[u32; 32]; 32])> {
         .collect()
 }
 
-fn write_to_log_event(
-    duration: time::Duration,
-    obj: f64,
-    layer_name: &str,
-    iter: usize,
-    updates: u64,
-) {
+fn write_to_log_event(duration: time::Duration, obj: f64, depth: usize, layer_name: &Path) {
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
@@ -48,8 +44,11 @@ fn write_to_log_event(
         .unwrap();
     writeln!(
         file,
-        "{}, {}, obj: {}, updates: {}, {}",
-        layer_name, iter, obj, updates, duration
+        "{}, {}, obj: {}, {}",
+        layer_name.to_str().unwrap(),
+        depth,
+        obj,
+        duration
     )
     .unwrap();
 }
@@ -61,6 +60,7 @@ type Pool2x2_32_64 = Conv2x2Stride2<u32, u128, [u128; 64], u64>;
 type Conv64 = Patch3x3NotchedConv<u64, [u128; 4], [[u128; 4]; 64], u64>;
 type Pool2x2_64_128 = Conv2x2Stride2<u64, [u128; 2], [[u128; 2]; 128], u128>;
 type Conv128 = Conv3x3<u128, [[[u128; 3]; 3]; 128], u128>;
+type DR64_32 = Conv1x1<u64, [u64; 32], u32>;
 
 trait Train<I, O, H> {
     fn train_new_layer(
@@ -76,27 +76,6 @@ trait Train<I, O, H> {
         head_update_freq: usize,
         depth: usize,
     );
-}
-
-macro_rules! train_layer {
-    ($layer_type:ty, $fs_path:expr, $examples:expr) => {{
-        let layer = <$layer_type>::new_from_fs(Path::new($fs_path)).unwrap_or_else(|| {
-            println!("{} not found, training", $fs_path);
-            let mut layer = <$layer_type>::new_from_split(&$examples);
-            let mut head = layer.make_pixel_head(&$examples);
-            let start = PreciseTime::now();
-            for i in 0..ITERS {
-                let iter_start = PreciseTime::now();
-                let obj = layer.optimize_layer(&mut head, &$examples, MOD);
-                write_to_log_event(iter_start.to(PreciseTime::now()), obj, $fs_path, i);
-                println!("{} obj: {}", i, obj);
-            }
-            println!("optimize time: {:?}", start.to(PreciseTime::now()));
-            layer.write_to_fs(Path::new($fs_path));
-            layer
-        });
-        layer.vec_apply(&$examples)
-    }};
 }
 
 impl<
@@ -115,23 +94,30 @@ where
         depth: usize,
     ) -> Vec<(usize, O)> {
         let layer = Self::new_from_fs(fs_path).unwrap_or_else(|| {
+            println!("{} not found, training", &fs_path.to_str().unwrap());
             let mut layer = Self::new_from_split(examples);
             let mut head = layer.make_pixel_head(examples);
             let start = PreciseTime::now();
             Self::train(&mut layer, &mut head, examples, head_update_freq, depth);
+            // uncomment for (small) acc improvment at the cost of double run time.
             //layer.optimize_layer::<PixelHead10<P>>(&mut head, &examples, head_update_freq);
             let (obj, updates) =
                 layer.optimize_layer::<PixelHead10<P>>(&mut head, &examples, head_update_freq);
+            //let new_examples: Vec<(usize, O)> = layer.vec_apply(examples);
+            //let obj = head.avg_objective(&new_examples);
             println!(
                 "obj: {}, depth: {}, {}",
                 obj,
                 depth,
                 start.to(PreciseTime::now())
             );
+            write_to_log_event(start.to(PreciseTime::now()), obj, depth, &fs_path);
+            layer.write_to_fs(&fs_path);
             layer
         });
         layer.vec_apply(&examples)
     }
+    // run time is ~constant with depth.
     fn train(
         layer: &mut Self,
         head: &mut PixelHead10<P>,
@@ -165,25 +151,33 @@ where
     }
 }
 const MOD: usize = 7;
-//const ITERS: usize = 20;
+const DEPTH: usize = 6;
 
 fn main() {
+    let base_path = Path::new("params/skip_con_test");
     write_to_log_event(
         PreciseTime::now().to(PreciseTime::now()),
         0f64,
-        &"starting train",
         0,
-        0,
+        &base_path.join("l0_c1_32/start"),
     );
-    let examples = load_data();
-    dbg!(examples.len());
-    let examples = Conv32::train_new_layer(&examples, Path::new("params/chan32_base/conv1_32.vecprms"), MOD, 6);
-    //let examples = train_layer!(Conv16, "params/batch_test/conv1.vecprms", examples);
-    //let examples = train_layer!(Conv16, "params/batch_test/conv2.vecprms", examples);
-    //let examples = train_layer!(Conv16, "params/batch_test/conv3.vecprms", examples);
-    //let examples = train_layer!(Conv16, "params/batch_test/conv4.vecprms", examples);
-    //let examples = train_layer!(Conv16, "params/batch_test/conv5.vecprms", examples);
+    let s0 = load_data();
+    dbg!(s0.len());
+    let s1 = Conv32::train_new_layer(&s0, &base_path.join("l1_c3_32"), MOD, DEPTH);
+
+    let cc0_1: Vec<(usize, [[u64; 32]; 32])> = vec_concat_examples(&s0, &s1);
+    let dr1 = DR64_32::train_new_layer(&cc0_1, &base_path.join("l2_c1_32"), MOD, DEPTH);
+    let s2 = Conv32::train_new_layer(&dr1, &base_path.join("l2_c3_32"), MOD, DEPTH);
+
+    let cc1_2: Vec<(usize, [[u64; 32]; 32])> = vec_concat_examples(&s1, &s2);
+    let dr2 = DR64_32::train_new_layer(&cc1_2, &base_path.join("l3_c1_32"), MOD, DEPTH);
+    let s3 = Conv32::train_new_layer(&dr2, &base_path.join("l3_c3_32"), MOD, DEPTH);
+
+    let cc2_3: Vec<(usize, [[u64; 32]; 32])> = vec_concat_examples(&s2, &s3);
+    let dr3 = DR64_32::train_new_layer(&cc1_2, &base_path.join("l4_c1_32"), MOD, DEPTH);
+    let s4 = Conv32::train_new_layer(&dr3, &base_path.join("l3_c3_32"), MOD, DEPTH);
 }
+
 // MOD: 20
 // obj: 0.1795872222222222,  depth: 3, PT965.950173838S
 // obj: 0.18120546666666665, depth: 4, PT961.309522735S
@@ -196,3 +190,10 @@ fn main() {
 // obj: 0.1842907111111111, depth: 6, PT1504.277525121S
 
 // obj: 0.19333304444444444, depth: 6, PT5386.694501952S
+
+//31 202 21.290213333333334%
+//31 252 21.290408888888887%
+//depth: 6, obj: 0.2129040888888889, updates: 573
+//starting optimize layer with 50000 examples
+//head update time: Duration { secs: 9, nanos: 376537054 }
+//0 1 21.283684444444443%
